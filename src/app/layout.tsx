6203c824a8ac7d4a61b26180efe50b5a2f61120f
guicalmeida <guica.almeida@gmail.com>
2023-01-26 17:27:07 -0300

    fix: universalize font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,27 @@
 import './globals.css'
-import Sidebar from '@/components/sidebar'
+import Sidebar from './(components)/sidebar'
+import { Nunito } from '@next/font/google'
+
+const nunito = Nunito({
+  subsets: ['latin'],
+})
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const options = [
+    {
+      name: 'create schema',
+      path: 'create-schema',
+    },
+  ]
   return (
-    <html lang="en">
+    <html lang="en" className={nunito.className}>
       <head />
       <body className="ml-[300px]">
-        <Sidebar />
+        <Sidebar options={options} />
         {children}
       </body>
     </html>
