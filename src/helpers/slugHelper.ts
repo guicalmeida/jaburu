9c5544a76ed5de2f46d5fc65eb3c6304cd37c5fe
guicalmeida <guica.almeida@gmail.com>
2023-02-02 20:22:24 -0300

    feat: create slug helper

---
+++ src/helpers/slugHelper.ts
@@ -0,0 +1,9 @@
+import slugify from 'slugify'
+
+export default function universalSlugify(string = '') {
+  return slugify(string, {
+    lower: true,
+    remove: /[.,:/"()]/g,
+    replacement: '_',
+  })
+}
