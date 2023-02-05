4fb8b18b22f7e98e3adf5689ba7acb4857f263be
guicalmeida <guica.almeida@gmail.com>
2023-02-05 16:42:43 -0300

    feat: dynamically list existing tables

--- src/app/(components)/sidebar.tsx
+++ src/app/(components)/sidebar.tsx
@@ -1,6 +1,20 @@
+import universalSlugify from '@/helpers/slugHelper'
 import Link from 'next/link'
 
-export default function Sidebar({ options }: Options) {
+const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/schemas`
+
+async function getTableNames() {
+  const res = await fetch(apiUrl, { method: 'GET' })
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+
+  return res.json()
+}
+
+export default async function Sidebar() {
+  const tableNames = await getTableNames()
   return (
     <div className="sidebar fixed top-0 bottom-0 left-0 w-[300px] overflow-y-auto bg-gray-900 p-2 text-center">
       <div className="text-xl text-gray-100">
@@ -11,27 +25,31 @@ export default function Sidebar({ options }: Options) {
         </div>
         <div className="my-2 h-[1px] bg-gray-600"></div>
       </div>
+      <div className="mt-6 flex items-center justify-between">
+        <h2 className="ml-3 text-[15px] font-bold text-gray-200">Schemas</h2>
+        <a
+          className="pr-5 text-lg font-bold"
+          aria-label="add new item"
+          href="create-schema"
+        >
+          +
+        </a>
+      </div>
 
-      {options?.map((option) => {
+      {tableNames?.map((tableName: string) => {
+        const slug = universalSlugify(tableName)
         return (
           <Link
             className="mt-3 flex cursor-pointer items-center rounded-md p-2.5 px-4 text-white duration-300 hover:bg-blue-600"
-            key={option.name}
-            href={option.path}
+            key={slug}
+            href={`schemas/${slug}`}
           >
             <span className="ml-4 text-[15px] font-bold text-gray-200">
-              {option.name}
+              {tableName}
             </span>
           </Link>
         )
       })}
     </div>
   )
-}
-
-interface Options {
-  options: {
-    name: string
-    path: string
-  }[]
 }
