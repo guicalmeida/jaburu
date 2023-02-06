e8665e56ba61189cc841525d46946fca448cb51f
guicalmeida <guica.almeida@gmail.com>
2023-02-06 19:51:43 -0300

    feat: create form page

--- src/app/schemas/[tableName]/page.tsx
+++ src/app/schemas/[tableName]/page.tsx
@@ -1,10 +1,19 @@
-export default function Page({ params }: params) {
-  const { tableName } = params
-  return <div>{tableName}</div>
-}
+'use client'
 
-type params = {
-  params: {
-    tableName: string
-  }
+import { ReactNode, useState } from 'react'
+import SchemaForm from './(components)/schema-form'
+
+export default function CreateSchema() {
+  const [showForm, setShowForm] = useState(false)
+  return (
+    <>
+      <button
+        className="m-6 rounded-md bg-emerald-600 py-2 px-8"
+        onClick={() => setShowForm(true)}
+      >
+        create field
+      </button>
+      {showForm && <SchemaForm />}
+    </>
+  )
 }
