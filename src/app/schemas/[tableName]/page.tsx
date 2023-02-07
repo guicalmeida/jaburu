06d621eb789f962a961ec07a631f7cde82c48cfa
guicalmeida <guica.almeida@gmail.com>
2023-02-07 18:17:55 -0300

    feat: show current fields in page

--- src/app/schemas/[tableName]/page.tsx
+++ src/app/schemas/[tableName]/page.tsx
@@ -1,10 +1,32 @@
 'use client'
 
-import { ReactNode, useState } from 'react'
+import { useSearchParams } from 'next/navigation'
+import { useEffect, useState } from 'react'
+import List from './(components)/list'
+import ListItem from './(components)/list-item'
 import SchemaForm from './(components)/schema-form'
+import { Column } from './(models)/columns.model'
 
-export default function CreateSchema() {
+export default function Page() {
+  const searchParams = useSearchParams()
+  const name = searchParams.get('name')
+
   const [showForm, setShowForm] = useState(false)
+  const [columns, setColumns] = useState<Column[]>([])
+  const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/${encodeURI(
+    name as string
+  )}`
+
+  useEffect(() => {
+    async function fetchColumns() {
+      const data = await fetch(apiUrl, { method: 'GET' })
+      const json = await data.json()
+      setColumns(json)
+    }
+
+    fetchColumns().catch(console.error)
+  }, [apiUrl])
+
   return (
     <>
       <button
@@ -14,6 +36,20 @@ export default function CreateSchema() {
         create field
       </button>
       {showForm && <SchemaForm />}
+      <h2>Current fields:</h2>
+      <List>
+        {columns.map((column) => {
+          const { columnName, required, type } = column
+          return (
+            <ListItem
+              columnName={columnName}
+              required={required}
+              type={type}
+              key={columnName}
+            />
+          )
+        })}
+      </List>
     </>
   )
 }
