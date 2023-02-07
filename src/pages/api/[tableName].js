95474d61310cc2fba3f0dc0b2ef0dc38b5849553
guicalmeida <guica.almeida@gmail.com>
2023-02-07 15:35:19 -0300

    feat: get table column info api

--- src/pages/api/[tableName].js
+++ src/pages/api/[tableName].js
@@ -43,5 +43,28 @@ export default async function handler(req, res) {
         console.log(err)
         res.end(JSON.stringify('error'))
       })
+  } else if (req.method === 'GET') {
+    const getAllColumns = async (knex) =>
+      await knex
+        .select('*')
+        .from('information_schema.columns')
+        .where({ table_name: tableName, table_schema: 'public' })
+
+    getAllColumns(database)
+      .then((columnsArr) => {
+        const columnsInfo = columnsArr.map((columnData) => {
+          const { column_name, is_nullable, udt_name } = columnData
+          return {
+            columnName: column_name,
+            required: !(is_nullable === 'YES'),
+            type: udt_name,
+          }
+        })
+        res.status(200).json(columnsInfo)
+      })
+      .catch((err) => {
+        console.log(err)
+        res.end(JSON.stringify('error'))
+      })
   }
 }
