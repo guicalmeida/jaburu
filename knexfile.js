8fa30805a2f577a199097e70db69c07cd226ceb9
guicalmeida <guica.almeida@gmail.com>
2023-02-10 17:15:28 -0300

    fix: change server configs

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,11 @@
 export const database = require('knex')({
   client: 'pg',
-  connection: process.env.DATABASE_URL,
+  connection: {
+    host: process.env.DATABASE_URL,
+    port: process.env.PORT,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE_NAME,
+  },
   searchPath: ['knex', 'public'],
 })
