bcd7723e823051b521ad76bf8ce328221ee4d309
guicalmeida <guica.almeida@gmail.com>
2023-02-14 18:24:00 -0300

    feat: add edit to id

--- src/pages/api/[tableName]/[id].js
+++ src/pages/api/[tableName]/[id].js
@@ -1,4 +1,5 @@
 import { database } from 'knexfile'
+import { getCurrentTime } from '@/helpers/timeHelper'
 
 export default async function handler(req, res) {
   const { tableName, id } = req.query
@@ -6,6 +7,21 @@ export default async function handler(req, res) {
     const getContentById = async (knex) => await knex(tableName).where({ id })
 
     getContentById(database)
+      .then((data) => {
+        res.status(200).json(data)
+      })
+      .catch((err) => {
+        console.log(err)
+        res.end(JSON.stringify('error'))
+      })
+  } else if (req.method === 'PUT') {
+    const timestamp = getCurrentTime()
+    const editContent = async (knex) =>
+      await knex(tableName)
+        .where({ id })
+        .update({ ...req.body, updated_at: timestamp })
+
+    editContent(database)
       .then((data) => {
         res.status(200).json(data)
       })
