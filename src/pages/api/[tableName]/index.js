3473be9413633843d9a124415ae7a5574354d0e3
guicalmeida <guica.almeida@gmail.com>
2023-02-14 15:49:55 -0300

    fix: dont destroy knex after finish

--- src/pages/api/[tableName]/index.js
+++ src/pages/api/[tableName]/index.js
@@ -50,9 +50,6 @@ export default async function handler(req, res) {
       .then((data) => {
         res.status(200).json(data)
       })
-      .finally(() => {
-        database.destroy()
-      })
       .catch((err) => {
         console.log(err)
         res.end(JSON.stringify('error'))
