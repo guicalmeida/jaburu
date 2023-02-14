6a2b54f1b34d79d0afd600315eda4fc303b1b81e
guicalmeida <guica.almeida@gmail.com>
2023-02-14 15:49:47 -0300

    feat: boostsrap table component

---
+++ src/app/content/[tableName]/(components)/contentTable.tsx
@@ -0,0 +1,5 @@
+'use client'
+
+export default function ContentTable() {
+  return <table></table>
+}
--- src/app/content/[tableName]/page.tsx
+++ src/app/content/[tableName]/page.tsx
@@ -1,9 +1,11 @@
 import ContentForm from './(components)/contentForm'
+import ContentTable from './(components)/contentTable'
 
 export default function Page() {
   return (
     <>
       <ContentForm />
+      <ContentTable />
     </>
   )
 }
