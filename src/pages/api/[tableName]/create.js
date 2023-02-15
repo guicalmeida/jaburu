e6a61810186cbe6cb7d6c01c5d20eecfaf98cb1e
guicalmeida <guica.almeida@gmail.com>
2023-02-15 19:31:37 -0300

    refactor: use imported function

--- src/pages/api/[tableName]/create.js
+++ src/pages/api/[tableName]/create.js
@@ -1,13 +1,11 @@
 import { database } from 'knexfile'
+import { insertIntoTable } from '../(common)'
 
 export default async function handler(req, res) {
   const { tableName } = req.query
 
   if (req.method === 'POST') {
-    const insertIntoTable = async (knex) =>
-      await knex(tableName).insert(req.body)
-
-    insertIntoTable(database)
+    insertIntoTable(database, tableName, req.body)
       .finally(() => {
         res.status(200).json('value created')
       })
