8beb8cc93c91572c0962b5b045f838bb8457bf20
guicalmeida <guica.almeida@gmail.com>
2023-02-15 19:32:10 -0300

    fix: register to metadata table new info

--- src/pages/api/schemas.js
+++ src/pages/api/schemas.js
@@ -1,22 +1,35 @@
 import { database } from 'knexfile'
+import { insertIntoTable } from './(common)'
+
+async function createDefaultTable(knex, tableName) {
+  return await knex.schema.createTable(tableName, (table) => {
+    table.increments()
+    table.timestamps(false, true)
+  })
+}
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    const { schemaName, apiID, pluralApiId } = await req.body
-    const createTable = async (knex) =>
-      await knex.schema
-        .createTable(schemaName, function (table) {
-          table.increments()
-          table.timestamps(false, true)
-        })
-        .finally(() => {
-          res.status(200).json('table successfully created')
-        })
-        .catch((err) => {
-          console.log(err)
-          res.end(JSON.stringify('error'))
-        })
-    createTable(database)
+    const { slug, display_name } = await req.body
+
+    if (slug.includes(' ')) {
+      return res.status(400).json({ error: 'spaces are not allowed in slug' })
+    }
+
+    const createTable = createDefaultTable(database, slug)
+    const registerTable = insertIntoTable(database, 'tables_metadata', {
+      slug,
+      display_name,
+    })
+
+    return await Promise.all([createTable, registerTable])
+      .then(() => {
+        return res.status(201).json('table successfully created')
+      })
+      .catch((err) => {
+        console.log(err)
+        return res.status(400).json('error')
+      })
   } else if (req.method === 'GET') {
     const getAllTables = async (knex) =>
       await knex
@@ -24,9 +37,14 @@ export default async function handler(req, res) {
         .from('information_schema.tables')
         .where({ table_schema: 'public' })
 
-    getAllTables(database).then((tableArr) => {
-      const namesArr = tableArr.map((names) => names['table_name'])
-      res.status(200).json(namesArr)
-    })
+    return await getAllTables(database)
+      .then((tableArr) => {
+        const namesArr = tableArr.map((names) => names['table_name'])
+        return res.status(200).json(namesArr)
+      })
+      .catch((err) => {
+        console.log(err)
+        return res.status(400).json('error')
+      })
   }
 }
