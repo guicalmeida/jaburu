4c0b51b6f78cf55dc229e8ab6c80278217c3f2f0
guicalmeida <guica.almeida@gmail.com>
2023-02-16 16:15:59 -0300

    refactor: turn content table into server comp

--- src/app/content/[tableName]/(components)/contentTable.tsx
+++ src/app/content/[tableName]/(components)/contentTable.tsx
@@ -1,8 +1,4 @@
-'use client'
-
-import universalSlugify from '@/helpers/slugHelper'
-import { columnMap } from '@/models/columns.model'
-import { use, useMemo } from 'react'
+import { Column } from '@/models/columns.model'
 import { formatUTC, isValidDate } from '@/helpers/timeHelper'
 import Link from 'next/link'
 
@@ -11,56 +7,62 @@ async function fetchContent(apiUrl: string) {
   return data.json()
 }
 
-export default function ContentTable({ slug }: { slug: string }) {
+async function fetchColumns(apiUrl: string) {
+  const data = await fetch(`${apiUrl}/columns`, { method: 'GET' })
+  return data.json()
+}
+
+function ContentRows(data: any[], slug: string) {
+  if (data.length > 0) {
+    return data.map((info: any) => (
+      <tr key={info.id} className="hover:bg-gray-50">
+        {Object.entries(info).map((cell: any) => (
+          <td key={cell[0]} className="px-6 py-4">
+            {isValidDate(cell[1]) ? formatUTC(cell[1]) : cell[1]}
+          </td>
+        ))}
+        <td className="px-6 py-4">
+          <Link href={`content/${slug}/${info.id}`}>Edit</Link>
+        </td>
+      </tr>
+    ))
+  } else {
+    return (
+      <tr className="hover:bg-gray-50">
+        <td className="px-6 py-4">Table has no entries</td>
+      </tr>
+    )
+  }
+}
+
+export default async function ContentTable({ slug }: { slug: string }) {
   const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/${slug}`
 
-  const contentResponse: any = use(fetchContent(apiUrl))
-  const columnsResponse = Object.keys(contentResponse[0] ?? {})
+  const content: any = await fetchContent(apiUrl)
+  const columns: any = await fetchColumns(apiUrl)
 
-  const data = useMemo(() => contentResponse, [contentResponse])
-
-  const columns: any = useMemo(
-    () =>
-      columnsResponse.map((col) => {
-        return {
-          Header: columnMap(col),
-          accessor: universalSlugify(col),
-        }
-      }),
-    [columnsResponse]
-  )
-
-  return (
-    <div className="m-5 overflow-hidden rounded-lg border border-gray-200 shadow-md">
-      <table className="w-full border-collapse bg-white text-left text-sm text-gray-500">
-        <thead className="bg-gray-50">
-          <tr>
-            {columns.map((col: any) => (
-              <th
-                key={col.accessor}
-                scope="col"
-                className="px-6 py-4 font-medium text-gray-900"
-              >
-                {col.Header}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody className="divide-y divide-gray-100 border-t border-gray-100">
-          {data.map((info: any) => (
-            <tr key={info.id} className="hover:bg-gray-50">
-              {Object.entries(info).map((cell: any) => (
-                <td key={cell[0]} className="px-6 py-4">
-                  {isValidDate(cell[1]) ? formatUTC(cell[1]) : cell[1]}
-                </td>
+  if (content.length > 0) {
+    return (
+      <div className="m-5 overflow-hidden rounded-lg border border-gray-200 shadow-md">
+        <table className="w-full border-collapse bg-white text-left text-sm text-gray-500">
+          <thead className="bg-gray-50">
+            <tr>
+              {columns.map((col: Column) => (
+                <th
+                  key={col.columnName}
+                  scope="col"
+                  className="px-6 py-4 font-medium text-gray-900"
+                >
+                  {col.columnName}
+                </th>
               ))}
-              <td className="px-6 py-4">
-                <Link href={`content/${slug}/${info.id}`}>Edit</Link>
-              </td>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  )
+          </thead>
+          <tbody className="divide-y divide-gray-100 border-t border-gray-100">
+            {ContentRows(content, slug)}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 }
