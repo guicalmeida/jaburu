813d7a6bdc65fd4b7b54e72e0fa55c699f5743e9
guicalmeida <guica.almeida@gmail.com>
2023-02-17 15:05:04 -0300

    fix: remover server component error

--- src/app/content/[tableName]/page.tsx
+++ src/app/content/[tableName]/page.tsx
@@ -5,6 +5,7 @@ export default function Page({ params }: { params: { tableName: string } }) {
   return (
     <>
       <ContentForm path={params?.tableName} />
+      {/* @ts-expect-error Server Component */}
       <ContentTable slug={params?.tableName} />
     </>
   )
