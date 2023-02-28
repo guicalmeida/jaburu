9911b3d1a48515b26af4c2e94c47006200aacf2f
guicalmeida <guica.almeida@gmail.com>
2023-02-28 18:38:43 -0300

    feat: add header to field edit

--- src/app/schemas/[tableName]/page.tsx
+++ src/app/schemas/[tableName]/page.tsx
@@ -2,6 +2,7 @@ import List from './(edit_schema_components)/list'
 import ListItem from './(edit_schema_components)/list-item'
 import ColumnForm from './(edit_schema_components)/column-form'
 import { Column } from '../../../models/columns.model'
+import Card from '@/components/Card'
 
 async function fetchTableMetadata(apiUrl: string) {
   const data = await fetch(apiUrl, { method: 'GET' })
@@ -18,6 +19,14 @@ export default async function Page({
 
   return (
     <>
+      <Card containerStyle="max-w-[550px]">
+        <h1 className="text-2xl  font-black">
+          Add columns to {tableMetadata?.display_name}
+        </h1>
+        <p className="pl-2 text-xl font-light">
+          How your content collection should be named?
+        </p>
+      </Card>
       <ColumnForm slug={params?.tableName} />
       <h2>Current fields:</h2>
       <List>
