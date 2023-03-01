530edee8bd6fdc5f0efe44b7114696710da54961
guicalmeida <guica.almeida@gmail.com>
2023-03-01 15:52:48 -0300

    feat: use new info structure

--- src/app/schemas/[tableName]/page.tsx
+++ src/app/schemas/[tableName]/page.tsx
@@ -1,8 +1,8 @@
 import Card from '@/components/Card'
 import ColumnForm from '@/components/column-form'
 import List from '@/components/list'
-import ListItem from '@/components/list-item'
-import { Column } from '@/models/columns.model'
+import ColumnListItem from '@/components/column-list-item'
+import { Column, typeMap } from '@/models/columns.model'
 
 async function fetchTableMetadata(apiUrl: string) {
   const data = await fetch(apiUrl, { method: 'GET' })
@@ -16,30 +16,42 @@ export default async function Page({
 }) {
   const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/schemas/${params?.tableName}`
   const tableMetadata = await fetchTableMetadata(apiUrl)
+  const { columns_metadata: columnsMetadata, display_name: displayName } =
+    tableMetadata
 
   return (
     <>
       <Card containerStyle="max-w-[550px]">
-        <h1 className="text-2xl  font-black">
-          Add columns to {tableMetadata?.display_name}
-        </h1>
+        <h1 className="text-2xl  font-black">Add columns to {displayName}</h1>
         <p className="pl-2 text-xl font-light">
-          How your content collection should be named?
+          Which fields should a {displayName} have?{' '}
         </p>
       </Card>
       <ColumnForm slug={params?.tableName} />
       <h2>Current fields:</h2>
       <List>
-        {tableMetadata?.columns?.map((column: Column) => {
-          const { columnName, required, type } = column
-          return (
-            <ListItem
-              columnName={columnName}
-              required={required}
-              type={type}
-              key={columnName}
-            />
-          )
+        {Object.entries<Column>(columnsMetadata).map((pair) => {
+          const {
+            required,
+            type,
+            display_name: displayName,
+            editable,
+            unique,
+          } = pair[1]
+
+          let itemInfo = typeMap(type)
+          if (required) itemInfo += ' · required'
+          if (unique) itemInfo += ' · unique'
+
+          if (editable) {
+            return (
+              <ColumnListItem
+                title={displayName}
+                info={itemInfo}
+                key={displayName}
+              />
+            )
+          }
         })}
       </List>
     </>
