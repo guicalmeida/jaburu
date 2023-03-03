c05eab3a75eeea3dd9dc4686c52f8187c8bccf5a
guicalmeida <guica.almeida@gmail.com>
2023-03-03 18:39:53 -0300

    fix: dont replace colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,17 +6,18 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    colors: {
-      jaburu: {
-        100: '#806996',
-        200: '#434984',
-        300: '#40354B',
-        400: '#2E2636',
+    extend: {
+      colors: {
+        jaburu: {
+          100: '#806996',
+          200: '#434984',
+          300: '#40354B',
+          400: '#2E2636',
+        },
+        white: '#FBFCFF',
+        disabled: '#999999',
       },
-      white: '#FBFCFF',
-      disabled: '#999999',
     },
-    extend: {},
   },
   plugins: [],
 }
