7515f68f3804bc63734b486420a15dc79b69f94b
guicalmeida <guica.almeida@gmail.com>
2023-03-04 13:39:58 -0300

    feat: update POST column API

--- src/pages/api/schemas/[tableName].js
+++ src/pages/api/schemas/[tableName].js
@@ -2,9 +2,13 @@ import { database } from 'knexfile'
 
 export default async function handler(req, res) {
   const { tableName } = req.query
+
   if (req.method === 'GET') {
     const getTableMetadata = async (knex) =>
-      await knex.select('*').from('tables_metadata').where({ tableName })
+      await knex
+        .select('*')
+        .from('tables_metadata')
+        .where({ api_id: tableName })
 
     return await getTableMetadata(database)
       .then((metadata) => res.status(200).json(metadata[0]))
@@ -13,40 +17,59 @@ export default async function handler(req, res) {
         return res.status(400).json({ error: 'error' })
       })
   } else if (req.method === 'POST') {
-    const { fieldName, fieldType, required } = await req.body
+    const {
+      display_name,
+      api_id: columnApiId,
+      type,
+      editable,
+      unique,
+      required,
+    } = await req.body
+
     const createColumn = async (knex) => {
-      switch (fieldType) {
-        case 'string':
+      switch (type) {
+        case 'text':
           return await knex.schema.alterTable(tableName, (table) => {
             if (required) {
-              table.string(fieldName).notNullable()
+              table.text(columnApiId).notNullable()
             } else {
-              table.string(fieldName).nullable()
+              table.text(columnApiId).nullable()
             }
-          })
-        case 'integer':
-          return await knex.schema.alterTable(tableName, (table) => {
-            if (required) {
-              table.integer(fieldName).notNullable()
-            } else {
-              table.integer(fieldName).nullable()
-            }
-          })
-        case 'markdown':
-          return await knex.schema.alterTable(tableName, (table) => {
-            if (required) {
-              table.text(fieldName).notNullable()
-            } else {
-              table.text(fieldName).nullable()
+            if (unique) {
+              table.unique(columnApiId)
             }
           })
       }
     }
 
-    return createColumn(database)
+    const getColumnsMetadata = async (knex) =>
+      await knex
+        .select('columns_metadata')
+        .from('tables_metadata')
+        .where({ api_id: tableName })
+
+    const setColumnsMetadata = async (knex) =>
+      await getColumnsMetadata(database).then(async (value) => {
+        const { columns_metadata } = value[0]
+        return await knex('tables_metadata')
+          .where({ api_id: tableName })
+          .update({
+            columns_metadata: {
+              ...columns_metadata,
+              [columnApiId]: {
+                display_name,
+                editable,
+                unique,
+                required,
+                type,
+              },
+            },
+          })
+      })
+
+    return Promise.all([createColumn(database), setColumnsMetadata(database)])
       .then((value) => {
-        console.log(value)
-        return res.status(201).json({ message: 'value created' })
+        return res.status(201).json({ message: 'column created' })
       })
       .catch((err) => {
         console.log(err)
