23c475b37cd31ff17fd1a6c89d3a327bce926229
guicalmeida <guica.almeida@gmail.com>
2023-03-04 12:26:23 -0300

    refactor: move column POST to schema API

--- src/pages/api/[tableName]/index.js
+++ src/pages/api/[tableName]/index.js
@@ -3,47 +3,7 @@ import { database } from 'knexfile'
 export default async function handler(req, res) {
   const { tableName } = req.query
 
-  if (req.method === 'POST') {
-    const { fieldName, fieldType, required } = await req.body
-    const createColumn = async (knex) => {
-      switch (fieldType) {
-        case 'string':
-          return await knex.schema.alterTable(tableName, (table) => {
-            if (required) {
-              table.string(fieldName).notNullable()
-            } else {
-              table.string(fieldName).nullable()
-            }
-          })
-        case 'integer':
-          return await knex.schema.alterTable(tableName, (table) => {
-            if (required) {
-              table.integer(fieldName).notNullable()
-            } else {
-              table.integer(fieldName).nullable()
-            }
-          })
-        case 'markdown':
-          return await knex.schema.alterTable(tableName, (table) => {
-            if (required) {
-              table.text(fieldName).notNullable()
-            } else {
-              table.text(fieldName).nullable()
-            }
-          })
-      }
-    }
-
-    return createColumn(database)
-      .then((value) => {
-        console.log(value)
-        return res.status(201).json({ message: 'value created' })
-      })
-      .catch((err) => {
-        console.log(err)
-        return res.status(400).json({ error: 'error' })
-      })
-  } else if (req.method === 'GET') {
+  if (req.method === 'GET') {
     const getAllPosts = async (knex) => await knex(tableName)
 
     return getAllPosts(database)
--- src/pages/api/schemas/[tableName].js
+++ src/pages/api/schemas/[tableName].js
@@ -12,5 +12,45 @@ export default async function handler(req, res) {
         console.log(err)
         return res.status(400).json({ error: 'error' })
       })
+  } else if (req.method === 'POST') {
+    const { fieldName, fieldType, required } = await req.body
+    const createColumn = async (knex) => {
+      switch (fieldType) {
+        case 'string':
+          return await knex.schema.alterTable(tableName, (table) => {
+            if (required) {
+              table.string(fieldName).notNullable()
+            } else {
+              table.string(fieldName).nullable()
+            }
+          })
+        case 'integer':
+          return await knex.schema.alterTable(tableName, (table) => {
+            if (required) {
+              table.integer(fieldName).notNullable()
+            } else {
+              table.integer(fieldName).nullable()
+            }
+          })
+        case 'markdown':
+          return await knex.schema.alterTable(tableName, (table) => {
+            if (required) {
+              table.text(fieldName).notNullable()
+            } else {
+              table.text(fieldName).nullable()
+            }
+          })
+      }
+    }
+
+    return createColumn(database)
+      .then((value) => {
+        console.log(value)
+        return res.status(201).json({ message: 'value created' })
+      })
+      .catch((err) => {
+        console.log(err)
+        return res.status(400).json({ error: 'error' })
+      })
   }
 }
