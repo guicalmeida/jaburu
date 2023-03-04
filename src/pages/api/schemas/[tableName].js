bd8cc878f9623c264a7c1f927459799a21c4a05a
guicalmeida <guica.almeida@gmail.com>
2023-03-04 12:26:04 -0300

    refactor: rename api placeholder

--- src/pages/api/schemas/[tableName].js
+++ src/pages/api/schemas/[tableName].js
@@ -1,10 +1,10 @@
 import { database } from 'knexfile'
 
 export default async function handler(req, res) {
-  const { api_id } = req.query
+  const { tableName } = req.query
   if (req.method === 'GET') {
     const getTableMetadata = async (knex) =>
-      await knex.select('*').from('tables_metadata').where({ api_id })
+      await knex.select('*').from('tables_metadata').where({ tableName })
 
     return await getTableMetadata(database)
       .then((metadata) => res.status(200).json(metadata[0]))
