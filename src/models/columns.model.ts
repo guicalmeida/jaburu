72a35590f8a82043381fbf00434003f97c726098
guicalmeida <guica.almeida@gmail.com>
2023-03-05 19:07:47 -0300

    fix: update type map

--- src/models/columns.model.ts
+++ src/models/columns.model.ts
@@ -8,14 +8,14 @@ export interface Column {
 
 export function typeMap(type: string) {
   switch (type) {
-    case 'int4':
-      return 'integer'
     case 'richText':
       return 'rich text'
     case 'timestamptz':
       return 'timestamp'
     case 'simpleText':
       return 'simple text'
+    default:
+      return type
   }
 }
 
