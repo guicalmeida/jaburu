c642a2872a98ca1934d6af8f2589eac39a63e206
guicalmeida <guica.almeida@gmail.com>
2023-03-07 17:56:00 -0300

    feat: create content form page

---
+++ src/app/content/[tableName]/create/page.tsx
@@ -0,0 +1,28 @@
+import Card from '@/components/Card'
+import ContentForm from '@/components/contentForm'
+
+async function getTableMetadata(tableName: string) {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/schemas/${tableName}`,
+    {
+      method: 'GET',
+    }
+  )
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+
+  return res.json()
+}
+
+export default function Page({ params }: { params: { tableName: string } }) {
+  return (
+    <>
+      <Card containerStyle="max-w-[550px]">
+        <h1 className="text-2xl font-black">Create new entry</h1>
+      </Card>
+      <ContentForm path={params.tableName} />
+    </>
+  )
+}
